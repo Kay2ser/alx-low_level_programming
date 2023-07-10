b8e838a08365c5ca0f7a855cb0e7696647994725
Kay2ser <kaysertony69@gmail.com>
2023-07-10 16:02:08 -0400

    C Programming in Linux Tutorial

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -40,7 +40,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer[bytes_read] = '\0';
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || (size_t)bytes_written != bytes_read)
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		close(fd);
 		free(buffer);
