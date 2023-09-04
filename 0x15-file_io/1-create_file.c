bd4f6560bb8c1870dece9601393df93cca0b6624
Kay2ser <kaysertony69@gmail.com>
2023-09-04 11:55:10 -0400

    file io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+
+/**
+ * create_file - Creates a file with the given filename and writes text_content
+ *               to it.
+ * @filename: The name of the file to create or truncate.
+ * @text_content: The content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written, permissions;
+
+	if (filename == NULL)
+		return (-1);
+
+	permissions = S_IRUSR | S_IWUSR; /* rw------- */
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
